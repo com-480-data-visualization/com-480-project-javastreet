341e364768247dc056301285a1bd80bdd9e82ed5
mael25 <mael18@windowslive.com>
2020-04-16 19:31:20 +0200

    auto-rotation

--- phoneV2/phone.js
+++ phoneV2/phone.js
@@ -73,6 +73,18 @@ class Phone {
 		contentDiv.style.marginTop = this.offsetY+padding+"px";
 	}
 
+
+	flip()
+	{
+		this.empty();
+		let parentDiv = document.getElementById(this.container).parentElement;
+		parentDiv.classList.toggle("phoneFormat");
+		parentDiv.classList.toggle("tabletFormat");
+		//We flip the ratio of the phone as well
+		this.ratio.reverse();
+		this.rescale();
+	}
+
 	//Object passed as argument as this will not be useful in onClick responses
 	update(phone)
 	{
@@ -118,104 +130,100 @@ class Phone {
 			// Update whether or not the elements are active
 			this.active = active;
 
-			let parentDiv = document.getElementById(phone.container).parentElement;
-			parentDiv.classList.toggle("phoneFormat");
-			parentDiv.classList.toggle("tabletFormat");
-			//We flip the ratio of the phone as well
-			phone.ratio.reverse();
-			console.log(phone.ratio);
-			phone.empty();
-			phone.rescale();
-		});
+			phone.flip();
+
+			let contentDiv = document.getElementById("content");
+			contentDiv.style.display = "inline-block";
+			//contentDiv.innerHTML='<object type="text/html" data="app.html"></object>'
+			contentDiv.innerHTML='<iframe class="frame" src="app.html"></iframe>'
+
+			phone.rescaleContent();
+	});
 
 
-		//Border of phone
-		svg.append('rect')
-		.attr('x', this.offsetX)
-		.attr('y', this.offsetY)
-		.attr('rx', 15)
-		.attr('ry', 15)
-		.attr('width', this.width)
-		.attr('height', this.height)
-		.style('stroke', 'grey')
-		.style('stroke-width', 5)
-		.style('fill','none');
+	//Border of phone
+	svg.append('rect')
+	.attr('x', this.offsetX)
+	.attr('y', this.offsetY)
+	.attr('rx', 15)
+	.attr('ry', 15)
+	.attr('width', this.width)
+	.attr('height', this.height)
+	.style('stroke', 'grey')
+	.style('stroke-width', 5)
+	.style('fill','none');
 
-		//Buttons
-		const spaceBtwButtons = 3/2*this.margin;
-		svg.append('circle')
-		.attr('cx', this.offsetX + this.width/2)
-		.attr('cy', this.offsetY + this.height-this.margin/2)
-		.attr('r', this.size/8)
-		.style('stroke', 'grey')
-		.style('stroke-width', 2)
-		.attr('class','highlight')
-		.on('click', function(){ let contentDiv = document.getElementById("content");
-														 contentDiv.style.display = "inline-block";
-														 //contentDiv.innerHTML='<object type="text/html" data="app.html"></object>'
-														 contentDiv.innerHTML='<iframe class="frame" src="app.html"></iframe>'
-
-	 												 	 phone.rescaleContent();
-													 });
+	//Buttons
+	const spaceBtwButtons = 3/2*this.margin;
+	svg.append('circle')
+	.attr('cx', this.offsetX + this.width/2)
+	.attr('cy', this.offsetY + this.height-this.margin/2)
+	.attr('r', this.size/8)
+	.style('stroke', 'grey')
+	.style('stroke-width', 2)
+	.attr('class','highlight')
 
 
 
 
-		const rectSize = 3*this.size/16;
-		svg.append('rect')
-		.attr('x', this.offsetX + this.width/2 + spaceBtwButtons - rectSize/2)
-		.attr('y', this.offsetY + this.height - this.margin/2 - rectSize/2)
-		.attr('width', rectSize)
-		.attr('height', rectSize)
-		.style('stroke', 'grey')
-		.style('stroke-width', 2)
-		.attr('class','highlight');
+
+	const rectSize = 3*this.size/16;
+	svg.append('rect')
+	.attr('x', this.offsetX + this.width/2 + spaceBtwButtons - rectSize/2)
+	.attr('y', this.offsetY + this.height - this.margin/2 - rectSize/2)
+	.attr('width', rectSize)
+	.attr('height', rectSize)
+	.style('stroke', 'grey')
+	.style('stroke-width', 2)
+	.attr('class','highlight');
 
 
-		const lineGenerator = d3.line()
-		.x(d => d.x)
-		.y(d => d.y);
+	const lineGenerator = d3.line()
+	.x(d => d.x)
+	.y(d => d.y);
 
-		const triWidth =  3*this.size/16;
-		const triHeight = this.size/4;
-		const triStart = [this.offsetX + this.width/2 - spaceBtwButtons + triWidth/2,
-											this.offsetY + this.height - this.margin/2 - triHeight/2];
+	const triWidth =  3*this.size/16;
+	const triHeight = this.size/4;
+	const triStart = [this.offsetX + this.width/2 - spaceBtwButtons + triWidth/2,
+		this.offsetY + this.height - this.margin/2 - triHeight/2];
 		const triData = [ {"x": triStart[0], "y": triStart[1]},
-											{"x": triStart[0], "y": triStart[1]+triHeight},
-										 	{"x": triStart[0]-triWidth, "y": triStart[1]+triHeight/2},
-										  {"x": triStart[0], "y": triStart[1]}];
+		{"x": triStart[0], "y": triStart[1]+triHeight},
+		{"x": triStart[0]-triWidth, "y": triStart[1]+triHeight/2},
+		{"x": triStart[0], "y": triStart[1]}];
 		svg.append("path")
 		.attr("d", lineGenerator(triData))
 		.style('stroke', 'grey')
 		.attr("stroke-width", 2)
 		.attr('class','highlight')
 		.on('click', function(){ let contentDiv = document.getElementById("content");
-														 //Empty it
-														 contentDiv.innerHTML = '';
-														 //Make it invisible so that the void div does not block click
-														 contentDiv.style.display = "none";
-													 });
+		//Empty it
+		contentDiv.innerHTML = '';
+		//Make it invisible so that the void div does not block click
+		contentDiv.style.display = "none";
+
+		phone.flip();
+	});
 
 
-		/*function highlight(d, i)
-		{
-			d3.select(this).style("fill", "orange");
-		}
+	/*function highlight(d, i)
+	{
+	d3.select(this).style("fill", "orange");
+}
 
-		function dehighlight(d, i) {
-			d3.select(this).style("fill", "none")
-		}*/
+function dehighlight(d, i) {
+d3.select(this).style("fill", "none")
+}*/
 
-		/*function handleMouseOver(d, i) {  // Add interactivity
-		// Use D3 to select element, change color and size
+/*function handleMouseOver(d, i) {  // Add interactivity
+// Use D3 to select element, change color and size
 
-		d3.select(this).style("fill", "orange");
-		d3.select(this).attr("width", this.width);
-	}
+d3.select(this).style("fill", "orange");
+d3.select(this).attr("width", this.width);
+}
 
-	function handleMouseOut(d, i) {
-	// Use D3 to select element, change color back to normal
-	d3.select(this).style("fill", "gray");
+function handleMouseOut(d, i) {
+// Use D3 to select element, change color back to normal
+d3.select(this).style("fill", "gray");
 }*/
 }
 empty()
